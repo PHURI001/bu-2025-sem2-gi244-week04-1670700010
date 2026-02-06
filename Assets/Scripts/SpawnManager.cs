75ddafce6c47390db26c08b56d8a32f1a2530729
PHURI001 <phuriwat.chat@gmail.com>
2026-02-06 11:10:09 +0700

    finish week4

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -2,9 +2,22 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public GameObject[] AnimalPrefabs;
+
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            float x = Random.Range(-10, 10);
+            int animalIndex = Random.Range(0, AnimalPrefabs.Length);
+            Instantiate(AnimalPrefabs[animalIndex],
+                new Vector3(x, 0, 20),
+                Quaternion.Euler(0, 180, 0));
+        }
+        else
+        {
+            Debug.Log($"null prefab at index {AnimalPrefabs}");
+        }
     }
 }
